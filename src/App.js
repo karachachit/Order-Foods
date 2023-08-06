bf27f1c50c05e3eb310c20a2e4d4fa67753f4496
karachachit <karachachit@gmail.com>
2023-08-06 11:14:56 +0600

    add meals summary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import { Header } from "./components/header/Header";
+import { MealsSummary } from "./components/meals-summary/MealsSummary";
 
 
 function App() {
 return <div>
   <Header />
+  <MealsSummary />
+
+  
   {/* <Header />
   <MealsSummary />
   <Meals />
